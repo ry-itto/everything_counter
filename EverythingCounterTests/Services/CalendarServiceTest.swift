e6b2bb0f1e2ca02f2af4653889f7808e73e68b57
ryoya_ito <horusu.dqx@gmail.com>
2019-06-02 01:07:50 +0900

    CalendarServiceTest実装

---
+++ EverythingCounterTests/Services/CalendarServiceTest.swift
@@ -0,0 +1,65 @@
+//
+//  CalendarServiceTest.swift
+//  EverythingCounterTests
+//
+//  Created by 伊藤凌也 on 2019/06/02.
+//  Copyright © 2019 ry-itto. All rights reserved.
+//
+
+import XCTest
+import RealmSwift
+@testable import EverythingCounter
+
+class CalendarServiceTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    /// カレンダーが正常に生成されること
+    func testGenerateCalendar_Success() {
+        // データ作成
+        let year = 2019
+        let month = 6
+        let counterID = UUID().uuidString
+        
+        // 実行
+        let service = CalendarService()
+        let result = service.generateCalendar(year: year, month: month, counterID: counterID)
+        
+        // 検証
+        XCTAssertEqual(result.count, 30)
+        XCTAssertEqual(result[0].month, month)
+        XCTAssertEqual(result[0].dayOfWeek, 6)
+        XCTAssertEqual(result[0].day, 1)
+        XCTAssertEqual(result[0].dayOfWeekStr, "土")
+    }
+    
+    /// カレンダーが正常に生成されること
+    /// カウントされた日にフラグが立っていること
+    func testGenerateCalendar_SuccessCounted() {
+        // データ作成
+        let today = Date()
+        let year = Calendar.current.component(.year, from: today)
+        let month = Calendar.current.component(.month, from: today)
+        let day = Calendar.current.component(.day, from: today)
+        
+        // カウンター登録
+        let counterService = CounterService()
+        counterService.addCounter(title: "Test")
+        let counter = counterService.update(counterService.findAll()[0], title: nil, value: 1, type: .increase)
+        
+        // 実行
+        let service = CalendarService()
+        let result = service.generateCalendar(year: year, month: month, counterID: counter.id)
+        
+        // 検証
+        XCTAssertTrue(result[day - 1].isCountedDay)
+        XCTAssertFalse(result[day - 2 == 0 ? day + 1 : day - 2].isCountedDay)
+    }
+}
