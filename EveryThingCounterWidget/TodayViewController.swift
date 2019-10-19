a10d8d4376a5f33eabe53dc595d9f102f90b82c1
ryoya ito <horusu.dqx@gmail.com>
2019-10-20 02:15:59 +0900

    ウィジェットのターゲットを追加

---
+++ EveryThingCounterWidget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  EveryThingCounterWidget
+//
+//  Created by 伊藤凌也 on 2019/10/20.
+//  Copyright © 2019 ry-itto. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
