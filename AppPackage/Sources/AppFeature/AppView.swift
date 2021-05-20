bc4ec64011d0b3d6cf40e7b0d6e414e56891cee7
ryoya ito <110ryoya@gmail.com>
2021-05-21 00:59:34 +0900

    Rename existing one to *Legacy, Create new Project, structure

---
+++ AppPackage/Sources/AppFeature/AppView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+public struct AppView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
---
+++ EverythingCounter/EverythingCounter/App.swift
@@ -0,0 +1,11 @@
+import AppFeature
+import SwiftUI
+
+@main
+struct EverythingCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
